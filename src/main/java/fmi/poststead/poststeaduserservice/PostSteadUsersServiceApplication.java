294d9179206cfc587b9f69976baef7be623b6449
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2022-11-30 17:21:59 +0200

    info: Project initialization.

---
+++ src/main/java/fmi/poststead/poststeaduserservice/PostSteadUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package fmi.poststead.poststeaduserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSteadUsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostSteadUsersServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fmi/poststead/poststeaduserservice/PostSteadUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fmi.poststead.poststeaduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSteadUsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
