ecec52a863b29bc8fc614ee105d2ad7737b8fa9a
Adrian Alexiu <adi.alexiu@gmail.com>
2023-01-05 12:40:48 +0200

    added CORS configuration

--- src/main/java/io/poststead/poststeaduserservice/controller/UserController.java
+++ src/main/java/io/poststead/poststeaduserservice/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/users")
 @AllArgsConstructor
+@CrossOrigin("*")
 class UserController {
 
     private final UserFacade userFacade;
